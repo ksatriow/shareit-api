9d072e56cce7fbad3bf9a7a300000fb56ca3ef90
ksatriow <andkukuh@gmail.com>
2021-09-15 13:40:06 +0700

    implement user controller, route and service

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const swaggerUi = require("swagger-ui-express");
 
 const app = express();
 const bodyParser = require("body-parser");
+const usersRoutes = require("./routes/userRoute");
 
 app.use(bodyParser.json());
+
+app.use("/users", usersRoutes);
 
 app.listen(3000, () => {
   console.log("I am ready to lisen you");
