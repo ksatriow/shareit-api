11c68f59d8e2dfd7ab2ea2298bc16e524d445795
ksatriow <andkukuh@gmail.com>
2021-09-15 13:46:15 +0700

    implement api docs using swagger

--- server.js
+++ server.js
@@ -8,6 +8,24 @@ const usersRoutes = require("./routes/userRoute");
 
 app.use(bodyParser.json());
 
+// Swagger 
+const swaggerOption = {
+  swaggerDefinition: (swaggerJsdoc.Options = {
+    info: {
+      title: "shareit",
+      description: "ShareIt API Documentation",
+      contact: {
+        name: "Developer",
+      },
+      servers: ["http://localhost:3000/"],
+    },
+  }),
+  apis: ["server.js", "./routes/*.js"],
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOption);
+app.use("/shareit-api", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.use("/users", usersRoutes);
 
 app.listen(3000, () => {
